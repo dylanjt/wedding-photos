b2169207a604dd6cd510fa269bce0e7f01688a91
Dylan Taylor <dylan@dt.engineering>
2022-06-29 19:08:45 -0400

    clickable header takes you to /

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 import '../styles/globals.css'
 
@@ -10,13 +11,17 @@ const App = ({ Component, pageProps }) => {
       </Head>
       <div className="bg-black/20 relative min-h-screen">
         <div className="py-4 bg-cover space-y-2 text-center shadow-xl shadow-black/50 text-orange-100 bg-black/40 backdrop-blur-sm sticky top-0 z-50">
-          <div className="text-2xl sm:text-4xl">Amara & Dylan</div>
-          <div className="text-md sm:text-xl text-orange-100">
-            <span className="text-lg pr-1 italic">Married on</span>
-            <span className="font-semibold">May 27, 2022</span>
-            <span className="text-ls px-1.5 italic">at</span>
-            <span className="font-semibold">The Market at Grelen</span>
-          </div>
+          <Link href="/">
+            <a className="block">
+              <div className="text-2xl sm:text-4xl">Amara & Dylan</div>
+              <div className="text-md sm:text-xl text-orange-100">
+                <span className="text-lg pr-1 italic">Married on</span>
+                <span className="font-semibold">May 27, 2022</span>
+                <span className="text-ls px-1.5 italic">at</span>
+                <span className="font-semibold">The Market at Grelen</span>
+              </div>
+            </a>
+          </Link>
         </div>
         <div className="shadow-lg">
           <Component {...pageProps} />
